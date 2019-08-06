eb5a79a4e4a91eebaca1a85777c95d47c73ea87d
Derek Jensen <jensendp@gmail.com>
2019-08-06 17:43:39 -0500

    Allow retrieval of a single post from the Api.

--- BlogApi/DataAccessLayer/Repositories/PostRepository.cs
+++ BlogApi/DataAccessLayer/Repositories/PostRepository.cs
@@ -7,14 +7,17 @@ namespace BlogApi.DataAccessLayer.Repositories
 {
     public interface IPostRepository: IRepository<Post>
     {
-        
+        Post GetBySlug(string slug);
     }
 
     public class PostRepository : Repository<Post>, IPostRepository
     {
         public PostRepository(IBlogDatabaseSettings settings) : base(settings, settings.PostsCollectionName)
         {
-            var indexOptions = new CreateIndexOptions();
+            var indexOptions = new CreateIndexOptions 
+            {
+                Unique = true
+            };
             var indexKeys = Builders<Post>.IndexKeys.Ascending(p => p.Slug);
             var indexModel = new CreateIndexModel<Post>(indexKeys, indexOptions);
             _entities.Indexes.CreateOne(indexModel);
@@ -23,6 +26,11 @@ namespace BlogApi.DataAccessLayer.Repositories
         public override List<Post> GetAll()
         {
             return _entities.Find(p => p.PublicationDate.HasValue).ToList();
+        }
+
+        public Post GetBySlug(string slug)
+        {
+            return _entities.Find(p => p.PublicationDate.HasValue).SortByDescending(p => p.PublicationDate).FirstOrDefault();
         }
     }
 }
--- BlogApi/ServiceLayer/Services/PostService.cs
+++ BlogApi/ServiceLayer/Services/PostService.cs
@@ -2,12 +2,14 @@ using System.Collections.Generic;
 using System.Linq;
 using BlogApi.DataAccessLayer.Repositories;
 using BlogApi.DTO;
+using BlogApi.Exceptions;
 
 namespace BlogApi.ServiceLayer.Services
 {
     public interface IPostService
     {
         List<PostSummary> GetPostSummaries();
+        Post GetPost(string slug);
     }
 
     public class PostService : IPostService
@@ -23,9 +25,21 @@ namespace BlogApi.ServiceLayer.Services
         {
             var posts = _postRepository.GetAll();
 
-            var summaries = posts.Select(p => new PostSummary { Id = p.Id, Title = p.Title, Teaser = p.Teaser, PublicationDate = p.PublicationDate.Value, Author = new Author { Id = p.Author.Id, Name = p.Author.Name, AvatarUrl = p.Author.AvatarUrl}, Likes = p.Likes.Count }).ToList();
+            var summaries = posts.Select(p => new PostSummary { Id = p.Id, Title = p.Title, Slug = p.Slug, Teaser = p.Teaser, PublicationDate = p.PublicationDate.Value, Author = new Author { Id = p.Author.Id, Name = p.Author.Name, AvatarUrl = p.Author.AvatarUrl}, Likes = p.Likes.Count }).ToList();
         
             return summaries;
+        }
+
+        public Post GetPost(string slug)
+        {
+            var post = _postRepository.GetBySlug(slug);
+
+            if(post == null)
+            {
+                throw new PostNotFoundException("slug", slug);
+            }
+
+            return new Post(post);
         }
     }
 }
