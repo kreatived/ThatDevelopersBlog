d58d41354205f007b896c8835741aa4e3de4ac6d
Derek Jensen <jensendp@gmail.com>
2019-08-29 12:54:30 -0500

    Add basic view post page.

---
+++ client/src/components/CreateCommentForm/CreateCommentForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const CreateCommentForm = () => {
+
+    return (
+        <div className="card">
+                <div className="card-header">
+                    <h4>Leave a comment</h4>
+                </div>
+            </div>
+    )
+}
